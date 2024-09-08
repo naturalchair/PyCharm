798ed7beacd3ab71796c41b7893a3ebe34b2a1ed
naturalchair <mattwyliu@gmail.com>
2024-09-07 20:02:38 -0400

    edit

--- hello.py
+++ hello.py
@@ -3,3 +3,7 @@ a = 6
 print("Edited from Github")
 print("Edited from py charm")
 print("main")
+
+# Code from feature 1 branch
+a = int(input("Enter an integer"))
+print("Your integer is " + str(a))
