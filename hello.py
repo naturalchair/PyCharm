9eb86cdf7209475041bff832bb2798cbb17f0cb1
naturalchair <mattwyliu@gmail.com>
2024-09-07 20:03:48 -0400

    Fixed comment in hello.py

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ print("Edited from Github")
 print("Edited from py charm")
 print("main")
 
-# Code from feature 1 branch
+# Code from feature-1 branch
 a = int(input("Enter an integer"))
 print("Your integer is " + str(a))
